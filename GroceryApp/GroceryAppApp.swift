7e5af7b055baa15982f72a6dc1e9c79964a9b7da
Dmitriy <58105647+vel9988@users.noreply.github.com>
2023-06-19 16:46:20 +0600

    Initial Commit

---
+++ GroceryApp/GroceryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryAppApp.swift
+//  GroceryApp
+//
+//  Created by Dmitryi Velko on 19.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct GroceryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
